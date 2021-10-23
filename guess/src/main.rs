78b5c04f66001f3c2926861715cad398024cc936
CatLava <evan.schoening@gmail.com>
2021-10-23 11:41:20 -0700

    error handling and loop for guessing game

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,12 +1,33 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
+
     println!("enter a guess");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1,101);
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    println!("Your secret number is: {}", secret_number);
 
-    println!("you guessed {}", guess);
+    loop {
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse()
+            .expect("please type a number");
+
+        println!("you guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("Winner!");
+                break;
+                }
+            }
+        }
 }
